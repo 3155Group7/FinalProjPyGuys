65d4fd9a3c016eaf7f7810915b57ca7df5b11c34
Reiley-Meeks <rmeeks10@icloud.com>
2021-11-17 15:13:10 -0500

    Name and Map Update
    Changing the name of the Danish Realm to Denmark so it will show on the map, and changing the kindom of the neatherlands to the neatherlands for a more concise name.

--- qLife.py
+++ qLife.py
@@ -9,26 +9,26 @@ import seaborn as sns
 data = pd.read_csv('assets/countries_quality_of_life_index.18-10-2021.csv')
 
 # Create World Map
-fig = px.choropleth(data_frame=data, locations='Name', locationmode='country names', color=' "QualityOfLifeIndex"')
+fig = px.choropleth(data_frame=data, locations='Name', locationmode='country names', color=' "Quality Of Life Index"')
 fig.show()
 
 # Create Bar Chart of 5 Nations
 plt.figure(figsize=(15, 6))
-sns.barplot(data=data.sort_values(by=' "QualityOfLifeIndex"', ascending=False).head(5), x='Name',
-            y=' "QualityOfLifeIndex"')
+sns.barplot(data=data.sort_values(by=' "Quality Of Life Index"', ascending=False).head(5), x='Name',
+            y=' "Quality Of Life Index"')
 plt.xticks(rotation=45)
 plt.show()
 
 # 10 Nation Bar Chart
 plt.figure(figsize=(15, 6))
-sns.barplot(data=data.sort_values(by=' "QualityOfLifeIndex"', ascending=False).head(10), x='Name',
-            y=' "QualityOfLifeIndex"')
+sns.barplot(data=data.sort_values(by=' "Quality Of Life Index"', ascending=False).head(10), x='Name',
+            y=' "Quality Of Life Index"')
 plt.xticks(rotation=45)
 plt.show()
 
 # 15 Nation Bar Chart
 plt.figure(figsize=(15, 6))
-sns.barplot(data=data.sort_values(by=' "QualityOfLifeIndex"', ascending=False).head(15), x='Name',
-            y=' "QualityOfLifeIndex"')
+sns.barplot(data=data.sort_values(by=' "Quality Of Life Index"', ascending=False).head(15), x='Name',
+            y=' "Quality Of Life Index"')
 plt.xticks(rotation=45)
 plt.show()
