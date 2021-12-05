88cdc10cf672fd3321ec8531201a492fd44d47e0
Firemrm <89547385+Firemrm@users.noreply.github.com>
2021-12-05 14:40:36 -0500

    Add files via upload

--- qLife.py
+++ qLife.py
@@ -147,7 +147,7 @@ def graph006():
 
     image = 'static/images/NCfig1.png'
     image4 = 'static/images/NCfig4.png'
-    return render_template('North-carolina.html', image1=image , image2=image4)
+    return render_template('North-carolina.html', image=image , image2=image4)
     
 
 
